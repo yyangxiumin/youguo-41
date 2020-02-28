1fff31f2fbebed2fe1bbb0375f9c947e2d0df3dc
yyangxiumin <yusjiy@163.com>
2020-02-28 23:45:38 +0800

    '搜索布局完成'

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-//app.js
+import request from "./utils/request.js";
+
 App({
   onLaunch: function () {
-   
+    request.defaults.baseURL = "https://api-hmugo-web.itheima.net/api/public/v1"
   }
 })
