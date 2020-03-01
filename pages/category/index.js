c8a5fb1722ba77b9552d8f0cd85eef9eeee27e45
yyangxiumin <yusjiy@163.com>
2020-03-01 22:59:36 +0800

    分类左侧菜单点击高亮

--- pages/category/index.js
+++ pages/category/index.js
@@ -4,13 +4,26 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+    // 记录当前点击了哪个菜单
+    current: 0,
   },
 
   /**
    * 生命周期函数--监听页面加载
    */
-  onLoad: function (options) {
+  onLoad: function(options) {
 
   },
+  // 点击切换左边菜单栏时候触发
+  handleClick(e) {
+    // index参数
+    const {
+      index
+    } = e.currentTarget.dataset;
+
+    // 保存当前点击的索引到data
+    this.setData({
+      current: index
+    })
+  }
 })
