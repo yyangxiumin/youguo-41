41a468c37654cd37fd15e3c7b9cb9cee39b36a83
yyangxiumin <yusjiy@163.com>
2020-03-01 19:29:32 +0800

    '导航菜单和楼层布局完成'

--- pages/index/index.js
+++ pages/index/index.js
@@ -3,7 +3,9 @@ import request from "../../utils/request.js"
 Page({
   data: {
     // 轮播图的数据
-    banners: []
+    banners: [],
+    // 菜单数组
+    menus: []
   },
 
   onLoad() {
@@ -12,10 +14,35 @@ Page({
       url: "/home/swiperdata"
     }).then(res => {
       // message是轮播图的数组
-      const { message } = res.data;
+      const {
+        message
+      } = res.data;
       // 赋值给banners
       this.setData({
         banners: message
+      })
+    })
+    request({
+      url: "/home/catitems"
+    }).then(res => {
+      // message是菜单的数组
+      const {
+        message
+      } = res.data;
+
+      // 循环添加跳转链接
+      const newData = message.map((v, i) => {
+        // 代表是分类
+        if (i === 0) {
+          v.url = "/pages/category/index"
+        }
+
+        return v;
+      })
+
+      // 赋值给menus
+      this.setData({
+        menus: newData
       })
     })
   }
